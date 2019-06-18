fbfcee807c285df31f760e37ab565e72e3af85be
Santiago Botero Sierra <sboteros@unal.edu.co>
2019-06-17 22:34:37 -0500

    Solved quiz and improved code

--- Quiz.R
+++ Quiz.R
@@ -5,7 +5,8 @@
 # Encoding: UTF-8
 # Date: 2019/06/17
 
-setwd("d:/Users/sbotero/Comisión Federal de Competencia Económica/Varios - General/DataScienceSpecialization/3 Getting and cleaning data/Week1/GettingAndCleaning_Week1")
+# setwd("d:/Users/sbotero/Comisión Federal de Competencia Económica/Varios - General/DataScienceSpecialization/3 Getting and cleaning data/Week1/GettingAndCleaning_Week1")
+setwd("C:/Users/sbote/OneDrive/Documentos/DataScienceSpecialization/3 Getting and cleaning data/GettingAndCleaning_Week1")
 if (!dir.exists("./data")) {dir.create("./data")}
 
 # 1. The American Community Survey distributes downloadable data about United 
@@ -17,12 +18,11 @@ if (!dir.exists("./data")) {dir.create("./data")}
 # How many properties are worth $1,000,000 or more?
 
   UrlFile <- "https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Fss06hid.csv"
-  download.file(UrlFile, "./data/IdahoHousing2016.csv")
-  IdahoHousing <- read.table("./data/IdahoHousing2016.csv",
-                             header = TRUE, sep = ",")
-  sum(IdahoHousing[IdahoHousing$VAL == 20 & !is.na(IdahoHousing$VAL), "WGTP"])
-  nrow(IdahoHousing[IdahoHousing$VAL == 20 & !is.na(IdahoHousing$VAL), ])
-  nrow(IdahoHousing[IdahoHousing$VAL == 20, ])  
+  if (!file.exists("./data/IdahoHousing2016.csv")) {
+    download.file(UrlFile, "./data/IdahoHousing2016.csv")
+  }
+  Idaho <- read.table("./data/IdahoHousing2016.csv", header = TRUE, sep = ",")
+  nrow(Idaho[Idaho$VAL == 24 & !is.na(Idaho$VAL), ])
 
 # 2. Use the data you loaded from Question 1. Consider the variable FES in the
 # code book. Which of the "tidy data" principles does this variable violate?
@@ -33,9 +33,14 @@ if (!dir.exists("./data")) {dir.create("./data")}
 #  https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2FDATA.gov_NGAP.xlsx
 #  Read rows 18-23 and columns 7-15 into R and assign the result to a variable
 #  called:
+  if (!require("xlsx")) {
+    install.packages("xlsx")
+  }
   library(xlsx)
   UrlFile <- "https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2FDATA.gov_NGAP.xlsx"
-  download.file(UrlFile, "./data/Gas.xlsx")
+  if (!file.exists("./data/Gas.xlsx")) {
+    download.file(UrlFile, "./data/Gas.xlsx", method = "curl")
+  }
   dat <- read.xlsx("./data/Gas.xlsx", 1, rowIndex = 18:23, colIndex = 7:15)
   
   # What is the value of:
@@ -44,27 +49,47 @@ if (!dir.exists("./data")) {dir.create("./data")}
 # 4. Read the XML data on Baltimore restaurants from here:
 # https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Frestaurants.xml
 # How many restaurants have zipcode 21231?
-
+  if (!require("XML")) {
+    install.packages("XML")
+  }
+  library(XML)
+  UrlFile <- "http://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Frestaurants.xml"
+  doc <- xmlTreeParse(UrlFile, useInternalNodes = TRUE)
+  rootNode <- xmlRoot(doc)
+  xmlName(rootNode)
+  names(rootNode)
+  Rest_Zip <- xpathSApply(rootNode, "//zipcode", xmlValue)
+  length(Rest_Zip[Rest_Zip == "21231"])
+  
 # 5. The American Community Survey distributes downloadable data about United
 # States communities. Download the 2006 microdata survey about housing for the
 # state of Idaho using download.file() from here:
 #  https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Fss06pid.csv
-#  using the fread() command load the data into an R object
   
-  DT
+  if (!file.exists("./data/Community.csv")) {
+    download.file("https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Fss06pid.csv",
+                  "./data/Community.csv")
+  }
+  
+#  using the fread() command load the data into an R object
+  if (!require("data.table")) {
+    install.packages("data.table")
+  }
+  library(data.table)
+  DT <- fread("./data/Community.csv")
 
 # The following are ways to calculate the average value of the variable
 # `pwgtp15` broken down by sex. Using the data.table package, which will deliver
 # the fastest user time?
-  rowMeans(DT)[DT$SEX==1]; rowMeans(DT)[DT$SEX==2]
+#  rowMeans(DT)[DT$SEX==1]; rowMeans(DT)[DT$SEX==2]
   
-  DT[,mean(pwgtp15),by=SEX]
+  system.time(DT[,mean(pwgtp15),by=SEX])
   
-  sapply(split(DT$pwgtp15,DT$SEX),mean)
+  system.time(sapply(split(DT$pwgtp15,DT$SEX),mean))
   
-  mean(DT[DT$SEX==1,]$pwgtp15); mean(DT[DT$SEX==2,]$pwgtp15)
+#  system.time(mean(DT[DT$SEX==1,]$pwgtp15); mean(DT[DT$SEX==2,]$pwgtp15))
   
-  mean(DT$pwgtp15,by=DT$SEX)
+  system.time(mean(DT$pwgtp15,by=DT$SEX))
   
-  tapply(DT$pwgtp15,DT$SEX,mean)
+  system.time(tapply(DT$pwgtp15,DT$SEX,mean))
   
--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # GettingAndCleaning_Week1
+
 Getting and cleaning data, Week 1 - Quiz, Data Science Specialization - Course 3
+
+# Functions
+
+The functions used to solve this quiz are:
+
++ `dir.exists` and `dir.create`
++ `require` and `install.packages`
++ `file.exists` and `download.file`
++ `read.table`, `read.xlsx` and `fread`
++ `xmlTreeParse`, `xmlName` and `xpathSApply`
+
+#  Formats
+
+The quiz needs the use of `.csv`, `.xlsx`, and `XML` data formats; the
+syllabus also contains information about `JSON` data.
+
+# Autorship
+  + Name: Santiago Botero S.
+  + E-mail: sboteros@unal.edu.co
+  + Encoding: UTF-8
+  + Date: 2019/06/17
